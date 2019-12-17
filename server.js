257050dcedf939fcb4ac4a401a8ad08cd55997ec
petewhartnett <petewhartnett@outlook.com>
2019-12-17 11:18:37 -0800

    completed contructor and api is working.

--- server.js
+++ server.js
@@ -5,35 +5,41 @@ const ejs = require('ejs');
 const superagent = require('superagent');
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 
-function GoogleBooks(books){
-    this.title = items.volumeInfo.author;
-    this.author = author;
+function GoogleBooks(books) {
+    this.title = books.title;
+    this.author = books.author;
 
 }
 
 app.post('/', (req, res) => {
-  const googleBookData =`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author} `
+    const googleBookData = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author} `
     superagent.get(googleBookData).then(bookData => {
-    const books = bookData.body.items.map(book => ({title: book.volumeInfo.title, author: book.volumeInfo.authors }));
+        //console.log(bookData);
+        const books = bookData.body.items.map(book => ({ title: book.volumeInfo.title, author: book.volumeInfo.authors }));
+       // console.log(books);
+        //var objectBlock = JSON.parse(JSON.stringify(books));
+        var objectArray = [];
+     
 
-    console.log(books);
+    books.map(newBook => objectArray.push(new GoogleBooks(newBook)));
+    console.log (objectArray);
 
-    res.render('book-results', {
-      books: books
+        res.render('book-results', {
+            objectArray: objectArray
+        });
     });
-  });
-
+   
 
 });
 
-app.listen(3000, () => console.log('lets look for books'));
+app.listen(3005, () => console.log('lets look for books'));
