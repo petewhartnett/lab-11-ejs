5164a498988aa90d83837d922281178b20ff250d
petewhartnett <petewhartnett@outlook.com>
2019-12-18 13:29:57 -0800

    Day two, data base set up

--- schema.sql
+++ schema.sql
@@ -11,3 +11,7 @@ CREATE TABLE bookshelf (
   bookshelf VARCHAR (255)
   );
 
+
+
+  INSERT INTO bookshelf(author, title, isbn, image_url, description, bookshelf)
+    Values('Nicholas G. Carr', 'The Shallows', '9', 'http://books.google.com/books/content?id=9-8jnjgYrgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'Expanding on an article that appeared in the Atlantic Monthly, the best-selling author of The Big Switch discusses the intellectual and cultural consequences of the Internet, and how it may be transforming our neural pathways for the worse.', 'Computers');
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const superagent = require('superagent');
 const pg = require('pg');
 
+require('dotenv').config();
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -15,15 +16,65 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
+app.post('/bookshelf', addDatabase);
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 
+
+function addDatabase(request, response) {
+    const instruction = `INSERT INTO
+    bookshelf (
+      author,
+      title,
+      isbn,
+      image_url,
+      description,
+      bookshelf
+    )
+  VALUES
+    ( $1, $2, $3, $4, $5, $6) RETURNING id;`;
+    const values = [request.body.author, request.body.title, request.body.isbn, request.body.image_url, request.body.description, request.body.bookshelf];
+    console.log(values);
+    client.query(instruction, values).then(sqlRes => {
+      response.redirect(`/bookshelf/${sqlRes.rows[0].id}`)
+    }).catch(e => errorHandler(e, response));
+  }
+
+// app.post('/', (req, res) => {
+//     const instruction = 'SELECT * FROM bookshelf;';
+//     client.query(instruction).then(function(sqlData){
+//      // console.log('YOOOOOOO',sqlData.rows);
+//       const bookArray = sqlData.rows;
+//     console.log(bookArray[1]);
+//       if(bookArray.length > 0){
+//         // res.send(bookArray.title);
+
+//         let testObject = {
+            
+//         }
+
+//         res.render('index', { todoArray });
+//       } else {
+//         res.render('book-results');
+//       }
+  
+//     });
+
+
+//   });
+
+
+
 function GoogleBooks(books) {
     this.title = books.title;
     this.author = books.author;
+    this.description = books.description || 'No description available';
+     this.img_url = books.imageLinks ? 'https' + books.imageLinks.thumbnail.slice(4) : '../img/book-icon.png'; //help from classmate for url portion 
+    this.isbn = books.industryIdentifiers ? `${books.industryIdentifiers[0].type} ${books.industryIdentifiers[0].identifier}` : 'No isbn available';
+    
 
 }
 app.get('/styles/styles.css', function(req, res){ res.send('/styles/styles.css'); res.end(); });
@@ -32,7 +83,7 @@ app.get('/styles/styles.css', function(req, res){ res.send('/styles/styles.css')
 
 app.post('/', (req, res) => {
     const googleBookData = `https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author} `
-    console.log(req.body.author)
+    //console.log(req.body.author)
     superagent.get(googleBookData).then(bookData => {
         //console.log(bookData);
         const books = bookData.body.items.map(book => ({ title: book.volumeInfo.title, author: book.volumeInfo.authors }));
@@ -42,7 +93,7 @@ app.post('/', (req, res) => {
      
 
     books.map(newBook => objectArray.push(new GoogleBooks(newBook)));
-    console.log (objectArray);
+   // console.log (objectArray);
 
         res.render('book-results', {
             objectArray: objectArray
@@ -78,4 +129,4 @@ app.get('/*', function(request, response){
     response.status(404).send('Try again!')
   })
   
-app.listen(3005, () => console.log('lets look for books'));
+app.listen(3025, () => console.log('lets look for books'));
